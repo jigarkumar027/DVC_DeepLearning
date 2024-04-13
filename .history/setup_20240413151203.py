b8c58d2ca8486f94bfa300dc91f283d9eba6145d
JIGARKUMAR PARMAR <parmarjigarkumar147@gmail.com>
2024-04-13 15:22:01 +0530

    config created

---
+++ .history/setup_20240413151203.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="jigarkumar027",
+    description="A small package for dvc DL pipeline",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jigarkumar027/DVC_DeepLearning",
+    author_email="parmarjigarkumar147@gmail.com",
+    packages=["src"],
+    python_requires=">=3.7",
+    install_requires=[
+        "dvc",
+        "tensorflow==2.3.4",
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "tqdm",
+        "PyYAML",
+        "boto3",
+    ]
+)
