986f52ea76f5dcda1a2e79231b502d4b69d4db68
JIGARKUMAR PARMAR <parmarjigarkumar147@gmail.com>
2024-04-13 17:48:02 +0530

    git ignore file updateq'

---
+++ .history/src/stage_01_load_save_20240413153817.py
@@ -0,0 +1,20 @@
+import os
+from src.utils.all_utils import read_yaml, create_directory
+import argparse
+import pandas as pd
+
+def get_data(config_path):
+    config = read_yaml(config_path)
+
+    source_donwload_dirs = config['source_download_dirs']
+    local_data_dirs = config['local_data_dirs']
+
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+
+    args.add_argument("--config","-c",default="config/config.yaml")
+
+    parsed_args = args.parse_args()
+
+    get_data(parsed_args.config)
